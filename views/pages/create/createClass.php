f72352f5191ffde9c42b3958d944e52113ec1ed2
Sofiane <makhouts@gmail.com>
2021-12-09 17:00:35 +0100

    placeholder adjustments

--- views/pages/create/createClass.php
+++ views/pages/create/createClass.php
@@ -7,7 +7,7 @@ require('views/headers/create/header.php');
     <form class='form' method= "POST" action="?class">
         <input type="text" name='name' placeholder='Class Name'>
         <input type="text" name='location' placeholder='Location'>
-        <input type="text" name='c_id' placeholder=''>
+        <input type="text" name='c_id' placeholder='ClassNumber'>
         <button name = "add">Submit</button>
     </form>
 </div>
