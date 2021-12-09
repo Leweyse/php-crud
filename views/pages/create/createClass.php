f5ab5cdd0a48809b01f75a3214ffe89f02f7fddb
zain Imtiaz <zain@zains-air.home>
2021-12-09 14:56:15 +0100

    its working nowwww

--- views/pages/create/createClass.php
+++ views/pages/create/createClass.php
@@ -3,11 +3,11 @@
 
 ?>
  <div class='container'>
-    <h2 class='title'>Create Class</h2>
-    <form class='form' action="" method= "POST">
+    <h2 class='title' >Create Class</h2>
+    <form class='form' method= "POST" action="?class">
         <input type="text" name='name' placeholder='Class Name'>
         <input type="text" name='location' placeholder='Location'>
         <input type="text" name='c_id' placeholder=''>
-        <button>Submit</button>
+        <button name = "add">Submit</button>
     </form>
 </div>
--- views/pages/edit/editClass.php
+++ views/pages/edit/editClass.php
@@ -1,14 +1,17 @@
 <?php 
-    require('../editComponents/header.php');
+ require('views/headers/overview/header.php');
 ?>
 
 <div class='container'>
      <h2 class='title'>Edit class</h2>
-     <form class='form' action="">
-        <input type="text" name='ClassName' placeholder='Class Name'>
-        <input type="text" name='location' placeholder='Location'>
-        <input type="text" name='classCode' placeholder='101-106'>
-        <button>Edit</button>
+     <form class='form' method="POST" action="?class">
+    
+     <input type="text" name="id" value=<?php echo $data['id'] ?> readonly>
+
+        <input type="text" name='name' placeholder='Class Name' value=<?php echo $data['info']['name']?>>
+        <input type="text" name='location' placeholder='Location' value=<?php echo $data['info']['location'] ?>>
+        <input type="text" name='c_id' placeholder='101-106'>
+        <button name="update">Edit</button>
     </form>
  </div>
 
